582740a1fbb889bc126565d7d41f4552e38307c6
JPathmakumar <pathmakumarjonathan@gmail.com>
2024-11-16 21:13:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.getElementById('btnScrollTop')
-    .addEventListener('click', function(){
-
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: "smooth"
-        });
-    }
-);
+alert("Welcome To My Website!!!");
